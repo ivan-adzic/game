21ca4aaf2ca71566cea851b59170c1b7de35c3fd
Ivan Adzic <adzicivan@gmail.com>
2023-07-15 13:38:08 +0200

    added points and life

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    list2.append(game.create_sprite(dot.get(LedSpriteProperty.X),
+    a_bullets.append(game.create_sprite(dot.get(LedSpriteProperty.X),
             dot.get(LedSpriteProperty.Y) - 1))
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
@@ -11,17 +11,42 @@ def on_button_pressed_b():
     dot.change(LedSpriteProperty.X, 1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-list2: List[game.LedSprite] = []
+a_bullets: List[game.LedSprite] = []
 dot: game.LedSprite = None
 dot = game.create_sprite(2, 4)
-list2 = []
+a_bullets = []
+a_enemies: List[game.LedSprite] = []
+game.set_life(3)
 
 def on_every_interval():
-    for value in list2:
-        if value.get(LedSpriteProperty.Y) == 0:
-            value.delete()
-            list2.remove_at(list2.index(value))
-        if not (value.is_deleted()):
-            value.change(LedSpriteProperty.Y, -1)
-    basic.show_number(len(list2))
-loops.every_interval(500, on_every_interval)
+    for enemie in a_enemies:
+        if enemie.get(LedSpriteProperty.Y) == 4:
+            enemie.delete()
+            a_enemies.remove_at(a_enemies.index(enemie))
+        if not (enemie.is_deleted()):
+            enemie.change(LedSpriteProperty.Y, 1)
+loops.every_interval(1000, on_every_interval)
+
+def on_every_interval2():
+    for bullet in a_bullets:
+        if bullet.get(LedSpriteProperty.Y) == 0:
+            bullet.delete()
+            a_bullets.remove_at(a_bullets.index(bullet))
+        if not (bullet.is_deleted()):
+            bullet.change(LedSpriteProperty.Y, -1)
+loops.every_interval(500, on_every_interval2)
+
+def on_every_interval3():
+    a_enemies.append(game.create_sprite(randint(0, 4), -1))
+    basic.pause(500)
+loops.every_interval(3000, on_every_interval3)
+
+def on_every_interval4():
+    for bullet2 in a_bullets:
+        for enemie2 in a_enemies:
+            if bullet2.is_touching(enemie2):
+                enemie2.delete()
+                a_enemies.remove_at(a_enemies.index(enemie2))
+                bullet2.delete()
+                a_bullets.remove_at(a_bullets.index(bullet2))
+loops.every_interval(100, on_every_interval4)
--- main.ts
+++ main.ts
@@ -11,6 +11,23 @@ let a_bullets: game.LedSprite[] = []
 let dot: game.LedSprite = null
 dot = game.createSprite(2, 4)
 a_bullets = []
+let a_enemies: game.LedSprite[] = []
+game.setLife(3)
+loops.everyInterval(1000, function () {
+    for (let enemie of a_enemies) {
+        if (enemie.get(LedSpriteProperty.Y) == 4) {
+            enemie.delete()
+            a_enemies.removeAt(a_enemies.indexOf(enemie))
+            game.removeLife(1)
+        }
+        if (!(enemie.isDeleted())) {
+            enemie.change(LedSpriteProperty.Y, 1)
+        }
+    }
+})
+/**
+ * Check if Bullet has reached the top of the screen
+ */
 loops.everyInterval(500, function () {
     for (let bullet of a_bullets) {
         if (bullet.get(LedSpriteProperty.Y) == 0) {
@@ -19,6 +36,23 @@ loops.everyInterval(500, function () {
         }
         if (!(bullet.isDeleted())) {
             bullet.change(LedSpriteProperty.Y, -1)
+        }
+    }
+})
+loops.everyInterval(3000, function () {
+    a_enemies.push(game.createSprite(randint(0, 4), -1))
+    basic.pause(500)
+})
+loops.everyInterval(100, function () {
+    for (let bullet of a_bullets) {
+        for (let enemie of a_enemies) {
+            game.addScore(1)
+            if (bullet.isTouching(enemie)) {
+                enemie.delete()
+                a_enemies.removeAt(a_enemies.indexOf(enemie))
+                bullet.delete()
+                a_bullets.removeAt(a_bullets.indexOf(bullet))
+            }
         }
     }
 })
