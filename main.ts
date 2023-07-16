f95178d42b3ed35832cc3f163b45abcbb01ffdb8
Ivan Adzic <adzicivan@gmail.com>
2023-07-16 12:22:27 +0200

    added serial-to-usb- com port and levels

--- main.ts
+++ main.ts
@@ -13,12 +13,15 @@ input.onButtonPressed(Button.B, function () {
 })
 let a_bullets: game.LedSprite[] = []
 let dot: game.LedSprite = null
-let onStartUp = true
+serial.redirectToUSB()
+serial.setBaudRate(BaudRate.BaudRate115200)
+serial.writeLine("Game Start")
 dot = game.createSprite(2, 4)
 a_bullets = []
 let a_enemies: game.LedSprite[] = []
 let life = 3
 let score = 0
+let level = 0
 // Check if Enemy has reached the bottom. 
 loops.everyInterval(1000, function () {
     for (let enemy of a_enemies) {
@@ -30,6 +33,25 @@ loops.everyInterval(1000, function () {
         if (!(enemy.isDeleted())) {
             enemy.change(LedSpriteProperty.Y, 1)
         }
+    }
+})
+// Check if Bullet has hit the Enemy.
+loops.everyInterval(10, function () {
+    for (let bullet of a_bullets) {
+        for (let enemy of a_enemies) {
+            if (bullet.isTouching(enemy)) {
+                enemy.delete()
+                a_enemies.removeAt(a_enemies.indexOf(enemy))
+                bullet.delete()
+                a_bullets.removeAt(a_bullets.indexOf(bullet))
+                score += 1
+                level = score / 10
+            }
+        }
+    }
+    if (life <= 0) {
+        game.setScore(score)
+        game.gameOver()
     }
 })
 // Check if bullet is out of the screen, if yes delete Bullet from the list.
@@ -49,21 +71,13 @@ loops.everyInterval(3000, function () {
     a_enemies.push(game.createSprite(randint(0, 4), -1))
     basic.pause(500)
 })
-// Check if Bullet has hit the Enemy.
+// Create Enemy every 3 seconds.
 loops.everyInterval(100, function () {
-    for (let bullet of a_bullets) {
-        for (let enemy of a_enemies) {
-            if (bullet.isTouching(enemy)) {
-                enemy.delete()
-                a_enemies.removeAt(a_enemies.indexOf(enemy))
-                bullet.delete()
-                a_bullets.removeAt(a_bullets.indexOf(bullet))
-                score += 1
-            }
-        }
-    }
-    if (life <= 0) {
-        game.setScore(score)
-        game.gameOver()
+    if (score % 10 == 0 && level != 0) {
+        game.pause()
+        basic.showString("Level")
+        basic.showNumber(level)
+        basic.pause(1000)
+        game.resume()
     }
 })
