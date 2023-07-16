c4a39618554adb1f954634f328e803316ecdef94
Ivan Adzic <adzicivan@gmail.com>
2023-07-16 09:16:21 +0200

    Code commented, added life and score variables.

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
+# Press button "A" to move Dot to the left.
+
 def on_button_pressed_a():
     dot.change(LedSpriteProperty.X, -1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+# Launch Bullet by pressing buttons "A+B" together.
+# TODO: Could be replaced by e.g. tilting device on "x" axe. 
 
 def on_button_pressed_ab():
     a_bullets.append(game.create_sprite(dot.get(LedSpriteProperty.X),
             dot.get(LedSpriteProperty.Y) - 1))
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
+# Press button "B" to move Dot to the right.
+
 def on_button_pressed_b():
     dot.change(LedSpriteProperty.X, 1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def on_gesture_shake():
+    global onStartUp
+    onStartUp = False
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
 a_bullets: List[game.LedSprite] = []
 dot: game.LedSprite = None
+onStartUp = False
+onStartUp = True
 dot = game.create_sprite(2, 4)
 a_bullets = []
 a_enemies: List[game.LedSprite] = []
-game.set_life(3)
+life = 3
+scoore = 0
+# Check if Enemy has reached the bottom. 
 
 def on_every_interval():
-    for enemie in a_enemies:
-        if enemie.get(LedSpriteProperty.Y) == 4:
-            enemie.delete()
-            a_enemies.remove_at(a_enemies.index(enemie))
-        if not (enemie.is_deleted()):
-            enemie.change(LedSpriteProperty.Y, 1)
+    global life
+    for enemy in a_enemies:
+        if enemy.get(LedSpriteProperty.Y) == 4:
+            enemy.delete()
+            a_enemies.remove_at(a_enemies.index(enemy))
+            life += -1
+        if not (enemy.is_deleted()):
+            enemy.change(LedSpriteProperty.Y, 1)
 loops.every_interval(1000, on_every_interval)
+
+# Check if bullet is out of the screen, if yes delete Bullet from the list.
 
 def on_every_interval2():
     for bullet in a_bullets:
@@ -36,17 +56,26 @@ def on_every_interval2():
             bullet.change(LedSpriteProperty.Y, -1)
 loops.every_interval(500, on_every_interval2)
 
+# Create Enemy every 3 seconds.
+
 def on_every_interval3():
     a_enemies.append(game.create_sprite(randint(0, 4), -1))
     basic.pause(500)
 loops.every_interval(3000, on_every_interval3)
 
+# Check if Bullet has hit the Enemy.
+
 def on_every_interval4():
+    global scoore
     for bullet2 in a_bullets:
-        for enemie2 in a_enemies:
-            if bullet2.is_touching(enemie2):
-                enemie2.delete()
-                a_enemies.remove_at(a_enemies.index(enemie2))
+        for enemy2 in a_enemies:
+            if bullet2.is_touching(enemy2):
+                enemy2.delete()
+                a_enemies.remove_at(a_enemies.index(enemy2))
                 bullet2.delete()
                 a_bullets.remove_at(a_bullets.index(bullet2))
+                scoore += 1
+    if life <= 0:
+        game.set_score(scoore)
+        game.game_over()
 loops.every_interval(100, on_every_interval4)
--- main.ts
+++ main.ts
@@ -1,33 +1,38 @@
+// Press button "A" to move Dot to the left.
 input.onButtonPressed(Button.A, function () {
     dot.change(LedSpriteProperty.X, -1)
 })
+// Launch Bullet by pressing buttons "A+B" together.
+// TODO: Could be replaced by e.g. tilting device on "x" axe. 
 input.onButtonPressed(Button.AB, function () {
     a_bullets.push(game.createSprite(dot.get(LedSpriteProperty.X), dot.get(LedSpriteProperty.Y) - 1))
 })
+// Press button "B" to move Dot to the right.
 input.onButtonPressed(Button.B, function () {
     dot.change(LedSpriteProperty.X, 1)
 })
 let a_bullets: game.LedSprite[] = []
 let dot: game.LedSprite = null
+let onStartUp = true
 dot = game.createSprite(2, 4)
 a_bullets = []
 let a_enemies: game.LedSprite[] = []
-game.setLife(3)
+let life = 3
+let score = 0
+// Check if Enemy has reached the bottom. 
 loops.everyInterval(1000, function () {
-    for (let enemie of a_enemies) {
-        if (enemie.get(LedSpriteProperty.Y) == 4) {
-            enemie.delete()
-            a_enemies.removeAt(a_enemies.indexOf(enemie))
-            game.removeLife(1)
+    for (let enemy of a_enemies) {
+        if (enemy.get(LedSpriteProperty.Y) == 4) {
+            enemy.delete()
+            a_enemies.removeAt(a_enemies.indexOf(enemy))
+            life += -1
         }
-        if (!(enemie.isDeleted())) {
-            enemie.change(LedSpriteProperty.Y, 1)
+        if (!(enemy.isDeleted())) {
+            enemy.change(LedSpriteProperty.Y, 1)
         }
     }
 })
-/**
- * Check if Bullet has reached the top of the screen
- */
+// Check if bullet is out of the screen, if yes delete Bullet from the list.
 loops.everyInterval(500, function () {
     for (let bullet of a_bullets) {
         if (bullet.get(LedSpriteProperty.Y) == 0) {
@@ -39,20 +44,26 @@ loops.everyInterval(500, function () {
         }
     }
 })
+// Create Enemy every 3 seconds.
 loops.everyInterval(3000, function () {
     a_enemies.push(game.createSprite(randint(0, 4), -1))
     basic.pause(500)
 })
+// Check if Bullet has hit the Enemy.
 loops.everyInterval(100, function () {
     for (let bullet of a_bullets) {
-        for (let enemie of a_enemies) {
-            game.addScore(1)
-            if (bullet.isTouching(enemie)) {
-                enemie.delete()
-                a_enemies.removeAt(a_enemies.indexOf(enemie))
+        for (let enemy of a_enemies) {
+            if (bullet.isTouching(enemy)) {
+                enemy.delete()
+                a_enemies.removeAt(a_enemies.indexOf(enemy))
                 bullet.delete()
                 a_bullets.removeAt(a_bullets.indexOf(bullet))
+                score += 1
             }
         }
+    }
+    if (life <= 0) {
+        game.setScore(score)
+        game.gameOver()
     }
 })
